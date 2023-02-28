ac282278744a3cd5d74eb90fc9335ff5fb9e03ba
Hernando Mosquera R <hmr07316@gmail.com>
2023-02-28 16:47:49 -0500

    proyect

--- src/components/UserTarj.jsx
+++ src/components/UserTarj.jsx
@@ -4,7 +4,7 @@ const UserTarj = () => {
     return (
         <div>
             <h1>GALLETAS DE LA FORTUNA</h1>
-            <img src="/fondo1.jpg" alt="" />
+            <img src="/profesor.jpg" alt="" />
         <ul>
             <li>Lo que sospechabas es verdad, procura ser amable con ellos, pues esta bondad ayudará a que todo mejore.</li>
             
